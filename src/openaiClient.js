0f711f54064b2c01cc6ee14d2bfdf61876d59ee9
vishxrad <kashyapvisharadyt@gmail.com>
2026-01-06 18:47:49 +0800

    fixed env

--- src/openaiClient.js
+++ src/openaiClient.js
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 
 export const openai = new OpenAI({
   baseURL: "https://api.tokenfactory.nebius.com/v1/",
-  apiKey: "v1.CmMKHHN0YXRpY2tleS1lMDBtZ3JjOTRiYzh3NXFmYmcSIXNlcnZpY2VhY2NvdW50LWUwMGEyOWhnMXhwMmo1OWt2ZDILCMHG7soGENuVjxw6DAjAyYaWBxDAno_BAUACWgNlMDA.AAAAAAAAAAG0GkKuZHrCTb1c4Bn7_VaoVx7K4_vOKHNyP1G76UJTsmqQrsM4QogomtsAV34ygq0OrZSFHRpD-P-zQPDk-6YG", 
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY, 
   dangerouslyAllowBrowser: true 
 });
+
